4924036996a1cca978cba1c3218b213f1e43a519
BelalAbandah <belalabandah@gmail.com>
2026-07-28 13:53:43 +0300

    fixing error in outteam page

--- app/[locale]/(root)/ourTeam/page.tsx
+++ app/[locale]/(root)/ourTeam/page.tsx
@@ -1,13 +1,12 @@
-// app/our-team/page.tsx
 import React from "react";
 import { getAllMembers } from "@/app/models/db/lib/services/outTeam";
 import OurTeamPage from "@/components/ourTeam/OurTeamPage";
-import type { Metadata } from "next"
-import { PAGE_METADATA, createMetadata } from "@/lib/constants/metadata"
-export const metadata: Metadata = createMetadata(PAGE_METADATA.ourTeam)
+import type { Metadata } from "next";
+import { PAGE_METADATA, createMetadata } from "@/lib/constants/metadata";
+export const metadata: Metadata = createMetadata(PAGE_METADATA.ourTeam);
 
 export default async function Page() {
-  const members = await getAllMembers();
+  const members = (await getAllMembers()).data;
 
   return (
     <>
